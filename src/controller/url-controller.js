7f4ccc0e9c7d88226defea79a5ea4dbe7191d6b5
dev-dongwon <dev.dongwonkim@gmail.com>
2019-11-15 15:08:06 +0900

    Feat : 요청 주소에 대해 리다이렉션하는 redirect Url 컨트롤러 메서드 추가
    shortUrl이 존재하면 리다이렉트
    그렇지 않을 경우 400 status code

--- src/controller/url-controller.js
+++ src/controller/url-controller.js
@@ -1,5 +1,5 @@
 const Url = require("../models/Url");
-const { hostUrl } = require('../config');
+const { hostUrl } = require("../config");
 
 const controller = {
   postUrl: async (req, res) => {
@@ -13,6 +13,24 @@ const controller = {
       return res.status(201).json({
         url: `${hostUrl}/${urlObj.shortUrl}`
       });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+
+  redirectUrl: async (req, res) => {
+    const shortUrl = req.params.shortUrl;
+
+    try {
+      const existUrl = await Url.findOne({
+        where: { shortUrl: shortUrl }
+      });
+
+      if (existUrl) {
+        return res.status(301).redirect(`${hostUrl}/${shortUrl}`);
+      }
+
+      return res.status(400).json({ msg: "요청 주소가 존재하지 않습니다" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
